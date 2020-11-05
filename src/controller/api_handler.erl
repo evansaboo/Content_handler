7b90251cec7e7837f2bcf8d4763d9c6115d0511a
saboo <saboo@kth.se>
2020-11-05 10:27:22 +0100

    More comments

--- src/controller/api_handler.erl
+++ src/controller/api_handler.erl
@@ -3,9 +3,12 @@
 -export([start_api_listener/0]).
 -export([stop_api_listener/0]).
 
-
-
+%% Function to start the Cowboy server, 
+%% add two endpoints and listen on port 8080
 start_api_listener() ->
+	
+	% Declare two API endpoints. Send incoming API calls to
+	% its corresponding module handler.
 	Dispatch = cowboy_router:compile([
 		{'_', [
 				{"/consumer", consumer_handler, []},
@@ -14,13 +17,14 @@ start_api_listener() ->
 		
 	]),
    
+	% Start the server and listen to port 8080
 	{ok, _} = cowboy:start_clear(http_listener, 
 		[{port, 8080}],
 		#{env => #{dispatch => Dispatch}}
 	).
 		
 	
-
+%% Function to stop the Cowboy server
 stop_api_listener() ->
     ok = cowboy:stop_listener(http).
 
