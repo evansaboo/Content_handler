e5352eb242c8b15b563fc7c8626cb4703801db2c
saboo <saboo@kth.se>
2020-11-05 10:21:01 +0100

    started commenting

--- src/kivra_app.erl
+++ src/kivra_app.erl
@@ -9,12 +9,18 @@
 
 -export([start/2, stop/1]).
 
-start(_StartType, _StartArgs) ->
-	api_handler:start_api_listener(),
-	
 
+%% Function to start the application called by rebar3 at execution time
+start(_StartType, _StartArgs) ->
+
+	% Start the cowboy server
+	api_handler:start_api_listener(),
 	
     kivra_sup:start_link().
 
+
+%% Function to stop the application called by rebar3 at execution time
 stop(_State) ->
+
+	%Stop the cowboy server
 	api_handler:stop_api_listener().
